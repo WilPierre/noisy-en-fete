6d2e9f28592b1910eacbf325936d893351e2eae2
WilPierre <wilauw@aol.com>
2026-06-03 17:18:04 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -529,7 +529,7 @@ function AdminView() {
 }
 
 // ─── PIN MODAL ────────────────────────────────────────────────────────────────
-const STAFF_PIN = '1234'; // Change ce code ici
+const STAFF_PIN = '2010'; // Change ce code ici
 
 function PinModal({ title, onSuccess, onClose }) {
   const [pin, setPin] = useState('');
